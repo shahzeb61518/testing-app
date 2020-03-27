67e2dfe17938350ec2766c05f601321f9057dd37
Shah zeb <34839860+shahzeb61518@users.noreply.github.com>
2020-03-27 16:08:43 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,30 +17,30 @@ mongoose.connect(url, (err, db) => {
 });
 
 
-// app.use(cors());
+ app.use(cors());
 // Add headers
 // res.setHeader('Access-Control-Allow-Origin', 'https://testing-app-shahzeb61518.herokuapp.com');
 // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-app.use(function (req, res, next) {
+//app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+ // res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
   // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Headers', 'XMLHttpRequest, X-Requested-With,Content-Type,Content-Length,Host,Authorization');
+  //res.setHeader('Access-Control-Allow-Headers', 'XMLHttpRequest, X-Requested-With,Content-Type,Content-Length,Host,Authorization');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+ // res.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
-  next();
-});
+  //next();
+//});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
