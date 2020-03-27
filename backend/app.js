cf233a581e4234a15adef25a694c93551bccb029
Shahzeb <shahzeb61518@gmail.com>
2020-03-27 19:18:59 +0500

    ...

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ mongoose.connect(url, (err, db) => {
 // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, application/json");
   next();
 });
 
