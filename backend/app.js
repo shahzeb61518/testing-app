a9b87aeb00cc2efb2a2a351e16cde4b5aa32c3f6
Shahzeb <shahzeb61518@gmail.com>
2020-03-27 18:56:59 +0500

    ...

--- backend/app.js
+++ backend/app.js
@@ -20,15 +20,11 @@ mongoose.connect(url, (err, db) => {
 //app.use(cors());
 // Add headers
 // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://testing-app-shahzeb61518.herokuapp.com');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
